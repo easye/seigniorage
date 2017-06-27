2eaa472c0681d09c3891b11b090cb475ad3cc944
Mark <evenson.not.org@gmail.com>
2017-06-27 13:45:14 +0200

    Tests now deserialize both the Ethereum and Bitcoin models

--- t/index.lisp
+++ t/index.lisp
@@ -9,5 +9,14 @@
   (prove:ok result
             "Testing reading of Bitcoin model…"))
 
+
+(prove:plan 1)
+(let ((result
+       (jeannie:read-rdf 
+        (asdf:system-relative-pathname :seigniorage
+                                       "model/ethereum.n3"))))
+  (prove:ok result
+            "Testing reading of Ethereum model…"))
+
 (prove:finalize)
 
